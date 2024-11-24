d759218a38748480baac35d54e6525112b1d5f49
chaitanya8108 <chaitanya81082430@gmail.com>
2024-11-25 00:41:25 +0530

    changes

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -26,7 +26,7 @@ const Header = () => {
         <div className="collapse navbar-collapse" id="navbarSupportedContent">
           <ul className="navbar-nav me-auto mb-2 mb-lg-0 ms-auto gap-lg-5 d-sm-flex">
             <li className="nav-item">
-              <Link className="nav-link" aria-current="page" to="/home">
+              <Link className="nav-link" aria-current="page" to="/actors">
                 Home
               </Link>
             </li>
