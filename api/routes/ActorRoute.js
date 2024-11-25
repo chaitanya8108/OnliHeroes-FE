a76f6b976d396a939636c4c09303870974e0e383
chaitanya8108 <chaitanya81082430@gmail.com>
2024-11-25 15:15:23 +0530

    changes

--- api/routes/ActorRoute.js
+++ api/routes/ActorRoute.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const actorController = require("../controllers/ActorController");
 
 //route for getting all actors
-router.get("/", actorController.getAllActors);
+router.get("/actors", actorController.getAllActors);
 
 //route for getting actors by id
 router.get("/:id", actorController.getActorsById);
